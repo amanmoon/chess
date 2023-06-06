56d0617cfe96713623ef28a38b8cc400c696720c
amanmoon <amanmoon099@gmail.com>
2023-06-06 10:42:20 +0530

    made the home page and fixed minor bugs

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
-export class HomepageComponent {
-
+export class HomepageComponent implements OnInit {
+  noOfPlayers: number = 2522550;
+  noOfGames: number=21420;
+  ngOnInit(){
+  //   setInterval(() => {
+  // this.noOfGames = fetch ;
+  //   }, 1000);
+  }
 }
